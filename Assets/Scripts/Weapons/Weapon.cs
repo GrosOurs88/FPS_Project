71409e2695efacea29be5e550209bdd7ce5b67be
Skaje <sorignet.gautrot.arthur@gmail.com>
2019-03-16 22:51:44 +0100

    Set-up Weapon.cs

--- Assets/Scripts/Weapons/Weapon.cs
+++ Assets/Scripts/Weapons/Weapon.cs
@@ -4,15 +4,29 @@ using UnityEngine;
 
 public class Weapon : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    //public float bulletDamage;
+    //public int magazineSize; //Taille du chargeur
+    //public int magazineAmmo; //nombre de balles restantes dans le chargeur
+    //public int carriedAmmo; //nombre de balles au total
+    
+    //public GameObject bullet;
+
+    void HitScanShot ()
     {
-        
+        //RayCast 
+        //if hit peux prendre des dégats
+        //transmettre à l'objet le nombre de points de vie correspondant (bulletDamage) // L'objet touché s'occupera de compter les degats etc.. 
     }
 
-    // Update is called once per frame
-    void Update()
+    void PhysicShot ()
     {
-        
+        //Instantier une bullet de la position de la caméra dans la direction de la caméra
+        //Enlever une balle du chargeur
+    }
+
+    void Reload ()
+    {
+        //if (carriedAmmo > 0)
+        //Recharche le chargeur mdeir
     }
 }
