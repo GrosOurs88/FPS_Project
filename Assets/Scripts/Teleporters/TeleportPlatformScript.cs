7569c1c953a83d165e902049a79048e611be17d2
Gros Ours <hugo.bourbon@wanadoo.fr>
2019-03-21 23:53:45 +0100

    Rangements des scripts, ajout d'un prefab de teleporteur, ajout d'une scene de test (quasiment vide pour le moment)

---
+++ Assets/Scripts/Teleporters/TeleportPlatformScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportPlatformScript : MonoBehaviour
+{
+    // Si l'avatar quitte le collider
+    public void OnTriggerExit(Collider other)
+    {
+        // Si c'est l'avatar qui se tient sur le téléporteur
+        if (other.tag == "Player")
+        {
+            // On réactive le collider central (qui permet la téléportation)
+            transform.Find("TeleportZone").gameObject.GetComponent<SphereCollider>().enabled = true;            
+        }
+    }
+}
---
+++ Assets/Scripts/Teleporters/TeleporterZoneScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleporterZoneScript : MonoBehaviour
+{
+    public GameObject elementToTeleportAt;
+    private GameObject zone;                        // Zone de la zone de teleportation au centre du téléporteur 
+    private SphereCollider teleportZone;            // Collider de la zone de teleportation au centre du téléporteur 
+
+    private void Start()
+    {
+        teleportZone = GetComponentInChildren<SphereCollider>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        // Si c'est l'avatar qui se tient sur le téléporteur
+        if (other.tag == "Player")
+        {
+            // Si le téléporteur est bien lié à un autre téléporteur
+            if (elementToTeleportAt.tag == "Teleporter")
+            {
+                // On désactive le sphere collider de la zone de teleport du téléporteur que l'on va atteindre
+                zone = elementToTeleportAt.transform.Find("TeleportZone").gameObject;
+                zone.GetComponent<SphereCollider>().enabled = false;
+
+                // L'avatar est téléporté sur l'autre téléporteur et récupère sa rotation
+                other.transform.position = new Vector3(elementToTeleportAt.transform.position.x, elementToTeleportAt.transform.position.y + 1, elementToTeleportAt.transform.position.z);
+                other.transform.rotation = elementToTeleportAt.transform.rotation;
+            }
+            else
+            {
+                Debug.LogError("The elementToTeleportAt of " + gameObject.name + "must be another Teleporter !");
+            }
+        }
+    }
+   
+}
