487844c0fac311f7deae820ae981f4b102e59dbe
Skaje <sorignet.gautrot.arthur@gmail.com>
2019-03-30 11:58:25 +0100

    Muzzle flash won't rotate...

---
+++ Assets/AlignWithParent.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlignWithParent : MonoBehaviour
+{
+    Camera cam;
+    public GameObject Ava;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = GetComponentInParent<Camera>();
+        print (cam.gameObject.name);
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        this.transform.rotation = Ava.transform.rotation;
+    }
+}
