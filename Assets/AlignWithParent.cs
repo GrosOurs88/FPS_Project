2874eea32ab95466dfa0877d119a42a270ade14d
Simon Albou <albou.simon@gmail.com>
2019-04-02 23:31:08 +0200

    Hi, your particles seem fixed :D

--- Assets/AlignWithParent.cs
+++ Assets/AlignWithParent.cs
@@ -4,19 +4,18 @@ using UnityEngine;
 
 public class AlignWithParent : MonoBehaviour
 {
-    Camera cam;
     public GameObject Ava;
-    // Start is called before the first frame update
+
+    ParticleSystem ps;
+
     void Start()
     {
-        cam = GetComponentInParent<Camera>();
-        // print (cam.gameObject.name);
-        
+        ps = GetComponent<ParticleSystem>();
     }
 
-    // Update is called once per frame
-    void LateUpdate()
+    void Update()
     {
-        this.transform.rotation = Ava.transform.rotation;
+        ParticleSystem.ShapeModule sm = ps.shape;
+        sm.rotation = Ava.transform.localEulerAngles;
     }
 }
