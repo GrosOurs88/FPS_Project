f3306f7154c7ee112d1ecbc4e2c60c6ae72e60f9
Gros Ours <hugo.bourbon@wanadoo.fr>
2019-04-04 19:57:20 +0200

    Ajout de deux prefabs d'armes (shotgun & Gun) + tri et rangement dans les dossiers

---
+++ Assets/Scripts/Weapons/EffectsScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EffectsScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void BigBullets()
+    {
+
+    }
+
+    public void ExplosionBullets()
+    {
+
+    }
+
+
+    public void 
+
+}
