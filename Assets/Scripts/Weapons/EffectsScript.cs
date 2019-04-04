46ad547055567b192b3d70ac57933350552173b9
Gros Ours <hugo.bourbon@wanadoo.fr>
2019-04-04 19:59:24 +0200

    Petit debug sur un nouveau script

--- Assets/Scripts/Weapons/EffectsScript.cs
+++ Assets/Scripts/Weapons/EffectsScript.cs
@@ -4,18 +4,16 @@ using UnityEngine;
 
 public class EffectsScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
     }
-
 
     public void BigBullets()
     {
@@ -27,7 +25,9 @@ public class EffectsScript : MonoBehaviour
 
     }
 
+    public void BouncingBullets()
+    {
 
-    public void 
+    }
 
 }
